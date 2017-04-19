d2afbfc55a664bf2c72d08882ce027bcae7b03e4
Anthony Ho <anthony.mh.ho@gmail.com>
2017-04-19 17:48:02 +0800

    Added dropOutliers()

---
+++ dropOutliers.R
@@ -0,0 +1,13 @@
+#### Function ####
+
+dropOutliers = function(numericVector) {
+  output = numericVector[!numericVector %in% boxplot.stats(numericVector)$out]
+  output
+}
+
+#### Example ####
+
+randomVector = c(1, 2, 1, 2, 6)
+
+randomVector
+dropOutliers(randomVector)
